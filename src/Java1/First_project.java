cf5e02672d16fe265e1de4b6a87eaa639af1ac8f
Malvika <malvikarasotra@gmail.com>
2024-02-06 16:25:04 +0530

    first commit

---
+++ src/Java1/First_project.java
@@ -0,0 +1,11 @@
+package Java1;
+
+public class First_project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("hye");
+	}
+
+}
